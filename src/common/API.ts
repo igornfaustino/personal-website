a2931f44ad0cedd8d9c99f34cd7f98f7e2afd10c
igornfaustino <igor@nfaustino.com>
2023-07-25 12:49:14 -0300

    refactor: extract env

---
+++ src/common/API.ts
@@ -0,0 +1,1 @@
+export const HYGRAPH_API = process.env.HYGRAPH_API as string
--- src/data/getAllPosts.ts
+++ src/data/getAllPosts.ts
@@ -1,10 +1,11 @@
+import { HYGRAPH_API } from '@/common/API'
 import { JsonResponseType } from '@/common/JsonResponseType'
 import { TIME } from '@/common/Time'
 
 const cacheTime = 10 * TIME.MINUTE
 
 export async function getAllPosts() {
-  const response = await fetch(process.env.HYGRAPH_API as string, {
+  const response = await fetch(HYGRAPH_API, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
