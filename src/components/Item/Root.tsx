f5044365f326890cdefd0e4b5bc8506e28924edd
igornfaustino <igor@nfaustino.com>
2023-09-21 18:33:49 -0300

    fix: item hover animation

--- src/components/Item/Root.tsx
+++ src/components/Item/Root.tsx
@@ -9,7 +9,7 @@ interface RootProps {
 export default function Root({ children, path }: RootProps) {
   if (!path) return <div className="py-3">{children}</div>
   return (
-    <div className="cursor-pointer rounded-md px-3 py-3 transition-all hover:bg-black/30 hover:backdrop-blur-sm">
+    <div className="cursor-pointer rounded-md py-3 transition-all hover:scale-[1.03]">
       <Link href={path}>{children}</Link>
     </div>
   )
