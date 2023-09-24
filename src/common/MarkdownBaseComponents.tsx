9b5e3da3e05edfc29a9ed891db760057691120bc
igornfaustino <igor@nfaustino.com>
2023-09-24 01:29:24 -0300

    fix: double underline problem

--- src/common/MarkdownBaseComponents.tsx
+++ src/common/MarkdownBaseComponents.tsx
@@ -1,5 +1,4 @@
 import { Code } from '@/components/CodeBlock'
-import ExternalLink from '@/components/ExternalLink'
 import Title from '@/components/Item/Title'
 import SubTitle from '@/components/SubTitle'
 import { ReactNode } from 'react'
@@ -9,9 +8,6 @@ export type ComponentProps = {
 }
 
 export const markdownBaseComponents = {
-  a: ({ children }: ComponentProps) => (
-    <ExternalLink className="border-b">{children}</ExternalLink>
-  ),
   h1: Title,
   h2: SubTitle,
   code: Code,
